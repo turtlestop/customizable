1ede93a492d2f479a1cbedce055a622c97003da6
Marco Dees <marcokdees@gmail.com>
2017-07-17 13:35:23 -0400

    changed database

--- login.php
+++ login.php
@@ -51,8 +51,8 @@ $_SESSION['login_user']=$username; // Initializing Session
 $_SESSION['pass'] = $password;
 $_SESSION['test'] = "green";
 
-$error = 'ok, SESSION[login_user] is ' + $_SESSION['login_user'] + ' and SESSION[pass] is ' + $_SESSION['pass']; 
-
+// $error = 'ok, SESSION[login_user] is ' + $_SESSION['login_user'] + ' and SESSION[pass] is ' + $_SESSION['pass'];
+$error = 'cooleo';
 // header("location: profile.php"); // Redirecting To Other Page
 }
  else {
